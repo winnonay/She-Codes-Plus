88afb1fea63fdc8593aa4bc7665f9f7e4e763157
winnonay <86921478+winnonay@users.noreply.github.com>
2021-12-10 14:17:26 -0500

    Plus Homework

---
+++ index.js
@@ -0,0 +1,40 @@
+//date-time
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hour = now.getHours();
+let minute = now.getMinutes();
+let current = document.querySelector("h2");
+current.innerHTML = `${day} ${hour}:${minute}`;
+
+//Seaching City & Temperature
+
+function showTemp(response) {
+  console.log(response.data.name);
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input-text").value;
+  search(city);
+}
+function search(city) {
+  let apiKey = "60462b3bc0c4ae404c2c0d6dfb41aa2b";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
