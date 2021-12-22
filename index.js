74d555ce2d712a49c4ad99b44bea438e1501525f
winnonay <86921478+winnonay@users.noreply.github.com>
2021-12-22 14:35:41 -0500

    icon update
    updated html/js with icon

--- index.js
+++ index.js
@@ -26,9 +26,15 @@ function showTemp(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function handleSubmit(event) {
   event.preventDefault();
